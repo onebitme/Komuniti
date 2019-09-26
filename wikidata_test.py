d93a203e899b807caef089f99b4f34a6a98008d0
onebitme <ersuns@outlook.com>
2019-09-27 01:00:01 +0300

    This is a test for both commit via pycharm and a python script for wikidata api test

---
+++ wikidata_test.py
@@ -0,0 +1,6 @@
+from qwikidata.entity import WikidataItem, WikidataLexeme, WikidataProperty
+from qwikidata.linked_data_interface import get_entity_dict_from_api
+
+Q_DOUGLAS_ADAMS = "Q42"
+q42_dict = get_entity_dict_from_api(Q_DOUGLAS_ADAMS)
+q42 = WikidataItem(q42_dict)
