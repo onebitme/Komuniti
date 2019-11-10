47cc401e9222ba398a02327aba5b8634c95bef33
onebitme <ersuns@gmail.com>
2019-11-11 01:44:24 +0300

    First .gitignore trial, Newpost form added

--- wikidata_test.py
+++ wikidata_test.py
@@ -1,8 +1,13 @@
 from qwikidata.entity import WikidataItem, WikidataLexeme, WikidataProperty
 from qwikidata.linked_data_interface import get_entity_dict_from_api
+import sqlparse
 
 Q_DOUGLAS_ADAMS = "Q42"
 q42_dict = get_entity_dict_from_api(Q_DOUGLAS_ADAMS)
 q42 = WikidataItem(q42_dict)
 
 print(q42)
+
+
+sqlparse.split('select * from foo; select * from bar;')
+[u'select * from foo; ', u'select * from bar;']
