5edfd25f8e2b6ba5eea535a781cd774b9e726edd
onebitme <ersuns@gmail.com>
2019-12-22 03:04:41 +0300

    Komuniti folder refactored to Komuniti_

--- Komuniti_/urls.py
+++ Komuniti_/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from komunitipage import views
-from Komuniti import settings
+from Komuniti_ import settings
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
--- Komuniti_/wsgi.py
+++ Komuniti_/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Komuniti project.
+WSGI config for Komuniti_ project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Komuniti.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Komuniti_.settings')
 
 application = get_wsgi_application()
